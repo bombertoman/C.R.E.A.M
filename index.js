cc46e31c3cca12fb19db6c5ae26064de98c57a47
bombertoman <68698061+bombertoman@users.noreply.github.com>
2024-12-11 13:57:30 +0100

    Rimossa chiamata app listen

--- index.js
+++ index.js
@@ -62,9 +62,3 @@ app.get( '/', ( req, res ) => {
 app.use( ( req, res ) => {
     res.status( 404 ).sendFile( path.join( __dirname, 'public/404.html' ) )
 })
-
-// Start server
-const PORT = 3000
-app.listen( PORT, () => {
-    console.log( `Server running on http://localhost:${PORT}` )
-} )
